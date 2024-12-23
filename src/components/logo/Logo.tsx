53b456307a0387d8ee669967ce945b18fbfb5bfa
Sasha Verlanov <phanton7487@gmail.com>
2024-12-23 19:39:59 +0300

    HTML

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Logo = () => {
+    return (
+        <a href="/">
+            <img className="header-logo" src="/logo.png" alt="logo"/>
+        </a>
+    );
+};
+
